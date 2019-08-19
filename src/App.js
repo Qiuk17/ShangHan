727fbc6a2aa20ebdc5f230db21312176e72e05b8
hydroph <qiu.ke@qq.com>
2019-08-19 22:51:57 +0800

    import antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button, WingBlank, WhiteSpace} from 'antd-mobile';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className = "App">
-        <header className = "App-header">
-          <img src = {logo} className = "App-logo" alt = "logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className = "App-link"
-            href = "https://reactjs.org"
-            target = "_blank"
-            rel = "noopener noreferer"
-            >
-              Learn React??
-            </a>
-        </header>
+        <WhiteSpace />
+        <WingBlank>
+          <Button loading>test</Button>
+        </WingBlank>
+        
       </div>
     );
   }
